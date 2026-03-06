3be50e5053d5c6b2718ac1095a3df92d632ad785
holymolyRon <wjdfhs111@gmail.com>
2026-03-06 17:12:00 +0900

    config: Vite, TS config 구성
    - Vite: resolve.alias
    - TS: baseUrl, paths

--- learning-react/src/app/App.tsx
+++ learning-react/src/app/App.tsx
@@ -1,7 +1,7 @@
 // import Header from '../components/header/Header';
 // import Main from '../components/main/Main';
 // import Footer from '../components/footer/Footer';
-import JsxExpression from '../jsx-expression/JsxExpression';
+import JsxExpression from '@/jsx-expression/JsxExpression';
 
 export default function App() {
   return (
--- learning-react/vite.config.ts
+++ learning-react/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    devSourcemap: true,
+  },
+  server: {
+    port: 3000,
+    open: false,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
