9969eda9b2e270adab3921e47b389f368ece74af
holymolyRon <wjdfhs111@gmail.com>
2026-03-06 17:08:47 +0900

    learn: 리액트 컴포넌트 분리 및 재사용
    - Header, Main, Footer, Image, FormField, Button

---
+++ learning-react/src/app/App.tsx
@@ -0,0 +1,15 @@
+// import Header from '../components/header/Header';
+// import Main from '../components/main/Main';
+// import Footer from '../components/footer/Footer';
+import JsxExpression from '../jsx-expression/JsxExpression';
+
+export default function App() {
+  return (
+    <>
+      <JsxExpression />
+      {/* <Header /> */}
+      {/* <Main /> */}
+      {/* <Footer /> */}
+    </>
+  );
+}
