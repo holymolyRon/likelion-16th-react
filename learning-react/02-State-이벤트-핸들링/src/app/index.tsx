a1a4c84400da96ffdbb1e6ba4a1a04c1771c255f
holymolyRon <wjdfhs111@gmail.com>
2026-03-09 11:31:23 +0900

    learn: Image 컴포넌트

--- learning-react/02-State-이벤트-핸들링/src/app/index.tsx
+++ learning-react/02-State-이벤트-핸들링/src/app/index.tsx
@@ -1,27 +1,19 @@
-import Footer from "@/components/Footer";
 import S from "./style.module.css";
-// import { createElement } from "react";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Image from "@/components/Image";
 
 export default function App() {
   return (
     <div className={S.container}>
-      <div data-placeholder />
+      <Header>
+        <h2 className={S.imageWrapper}>
+          <Image src="/react.svg" alt="" width={32} height={32} />
+          컴포넌트 디자인
+        </h2>
+      </Header>
+
       <Footer slogan={"완주 이후엔 스스로 학습이 가능!"} />
-
-      {/* Vite (with TypeScript) 컴파일 결과 (JSX => JavaScript)
-        jsxDEV(Footer, { slogan: ["완주 이후엔", "스스로 학습이 가능!"] }, void 0, false, {
-          fileName: "/Users/yamoo9/Desktop/likelion-16th-react/learing-react/02-State-이벤트-핸들링/src/app/index.tsx",
-          lineNumber: 9,
-          columnNumber: 7
-        }, this)
-      */}
-
-      {/* {createElement(
-        // 컴포넌트 이름 (Component Name)
-        Footer,
-        // 컴포넌트 속성 (Component Props)
-        { slogan: "완주 이후엔 스스로 학습 가능!" },
-      )} */}
     </div>
   );
 }
