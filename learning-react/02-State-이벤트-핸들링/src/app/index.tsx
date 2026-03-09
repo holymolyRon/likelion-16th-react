55e80dc4407b05e461fc437d13f4b82316fd5856
holymolyRon <wjdfhs111@gmail.com>
2026-03-09 13:48:43 +0900

    learn: 컴포넌트 설계

--- learning-react/02-State-이벤트-핸들링/src/app/index.tsx
+++ learning-react/02-State-이벤트-핸들링/src/app/index.tsx
@@ -2,6 +2,7 @@ import S from "./style.module.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Image from "@/components/Image";
+import Section from "@/components/Section";
 
 export default function App() {
   return (
@@ -12,6 +13,10 @@ export default function App() {
           컴포넌트 디자인
         </h2>
       </Header>
+
+      <Section title="리액트는 리액션(반응)!" isShowTitle>
+        <p>리액트는 사용자의 행동에 따라 리액션을 제공한다.</p>
+      </Section>
 
       <Footer slogan={"완주 이후엔 스스로 학습이 가능!"} />
     </div>
