a68afcb11334c246b6631b61e789b5b9848c60e7
holymolyRon <wjdfhs111@gmail.com>
2026-03-09 10:01:28 +0900

    learn: 데이터 바인딩

--- learning-react/02-State-이벤트-핸들링/src/components/Footer/index.tsx
+++ learning-react/02-State-이벤트-핸들링/src/components/Footer/index.tsx
@@ -1,12 +1,14 @@
-import S from './style.module.css'
+import S from "./style.module.css";
 
 export default function Footer() {
   /**
-   * 데이터 바인딩 (Data Binding)
+   * 데이터 바인딩 (Data Binding = Data + JSX = Markup)
    * - JavaScript 데이터(변수, 계산된 값 등)
    * - JSX 구문 안에 데이터 끼워넣기(Interpolation)
    */
-  const currentYear = 2026
+
+  // 숫자 값이 아닌, 날짜 객체 현재 년도 가져오기
+  const currentYear = new Date().getFullYear();
 
   /**
    * Props로 데이터 전달(부모 → 자식)
@@ -14,19 +16,20 @@ export default function Footer() {
    * - React.createElement API에서 Props
    * - Props 타입 정의 (인라인 → 인터페이스)
    */
-  const slogan = '완벽보다 완주를!'
+  const slogan = "완벽보다 완주를!";
 
   /**
    * 접근성 고려
    * - 사용자가 이해하기 쉽고 읽기 쉽도록 저작권 레이블(label) 구성
    */
-  const copyrightLabel = '모든 저작권은 이듬(EUID)에 있습니다. 완벽보다 완주를!'
+  const copyrightLabel = `모든 저작권은 이듬(EUID)에 있습니다. ${slogan}`;
 
   return (
     <footer className={S.footer}>
-      <small>
-        COPYRIGHT RESERVED. © <abbr title="이듬(EUID)">EUID</abbr>.
+      <small aria-label={copyrightLabel}>
+        {currentYear}COPYRIGHT RESERVED. © <abbr title="이듬(EUID)">EUID</abbr>
+        . {slogan}
       </small>
     </footer>
-  )
+  );
 }
