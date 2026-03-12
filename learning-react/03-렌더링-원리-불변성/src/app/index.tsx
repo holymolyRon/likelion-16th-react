4bd0094059fbe7f2990332a281a4052b814087de
holymolyRon <wjdfhs111@gmail.com>
2026-03-12 13:45:59 +0900

    learn: 유저 폼 제어 (Controlled)
    - 상태 초기화를 위한 예제 구성

--- learning-react/03-렌더링-원리-불변성/src/app/index.tsx
+++ learning-react/03-렌더링-원리-불변성/src/app/index.tsx
@@ -1,10 +1,10 @@
-import { Reconciliation } from "@/learns";
 import S from "./style.module.css";
+import { StateInitialization } from "@/practices";
 
 export default function App() {
   return (
     <div className={S.container}>
-      <Reconciliation />
+      <StateInitialization />
     </div>
   );
 }
--- learning-react/03-렌더링-원리-불변성/src/practices/StateInitialization/index.tsx
+++ learning-react/03-렌더링-원리-불변성/src/practices/StateInitialization/index.tsx
@@ -1,5 +1,30 @@
-import S from './style.module.css'
+import { useState } from "react";
+import S from "./style.module.css";
+import UserForm from "./UserForm";
 
-export default function App() {
-  return <div className={S.container}></div>
+// 부모(상위) 컴포넌트
+export default function StateInitialization() {
+  const [resetComponent, setResetComponent] = useState("");
+
+  return (
+    <div className={S.container}>
+      <header className={S.header}>
+        <h2>
+          회원가입 <span>(컴포넌트 초기화 Key: {resetComponent})</span>
+        </h2>
+        <button
+          type="button"
+          className={S.resetButton}
+          onClick={() => setResetComponent(resetComponent + "🌈")}
+        >
+          폼 초기화
+        </button>
+      </header>
+
+      {/* 자식(하위) 컴포넌트
+          이 내부의 상태에 접근 불가능
+          그러면 어떻게 하위 컴포넌트의 상태를 초기화할 수 있을까? */}
+      <UserForm key={resetComponent} />
+    </div>
+  );
 }
