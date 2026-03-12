0b8ef5f0ca8b8869354cd460a7944baf11960313
holymolyRon <wjdfhs111@gmail.com>
2026-03-12 14:04:46 +0900

    add: 04, 05 학습 코드

--- learning-react/04-리스트-렌더링-설계/src/app/index.tsx
+++ learning-react/04-리스트-렌더링-설계/src/app/index.tsx
@@ -1,10 +1,10 @@
-import { StaffListWithFilter } from '@/components'
+import ProductPage from '@/practices/02-filtering/pages/product'
 import S from './style.module.css'
 
 export default function App() {
   return (
     <div className={S.container}>
-      <StaffListWithFilter />
+      <ProductPage />
     </div>
   )
 }
