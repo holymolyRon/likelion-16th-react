2dec97ebd10fcddf8986f2249df10aa341309146
holymolyRon <wjdfhs111@gmail.com>
2026-03-13 09:37:39 +0900

    learn: 복잡한 데이터를 기반으로 스케쥴 테이블 (리스트 렌더링)

--- learning-react/04-리스트-렌더링-설계/src/components/ScheduleTable/type/schedule.ts
+++ learning-react/04-리스트-렌더링-설계/src/components/ScheduleTable/type/schedule.ts
@@ -1,17 +1,17 @@
-export type ShiftType = '오전' | '오후' | '야간'
+export type ShiftType = "오전" | "오후" | "야간";
 
 export interface Shift {
-  id: string
-  type: ShiftType
-  staffName: string
-  startTime: string
-  endTime: string
-  isCompleted: boolean
+  id: string;
+  type: string;
+  staffName: string;
+  startTime: string;
+  endTime: string;
+  isCompleted: boolean;
 }
 
 export interface DailySchedule {
-  id: string
-  date: string
-  dayOfWeek: string
-  shifts: Shift[]
+  id: string;
+  date: string;
+  dayOfWeek: string;
+  shifts: Shift[];
 }
