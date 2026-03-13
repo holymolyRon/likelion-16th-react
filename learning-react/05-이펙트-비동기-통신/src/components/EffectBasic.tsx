4f22d715ae8e6f7a0613eb2d71334f7ae691124e
holymolyRon <wjdfhs111@gmail.com>
2026-03-13 15:29:58 +0900

    (05) learn: 이펙트(Effect) 기초
    - 순수 함수 (리액트는 컴포넌트를 순수하게 유지하길 요구)
    - StrictMode의 역할 (개발 모드에서만 두 번 컴포넌트 실행해 순수한지 여부를 확인하여 문제가 발생할 수 있는 가능성을 개발자에게 안내)
    - 이펙트(설정함수): 랜더링 단계에서 벗어난 외부 시스템과 동기화 로직

--- learning-react/05-이펙트-비동기-통신/src/components/EffectBasic.tsx
+++ learning-react/05-이펙트-비동기-통신/src/components/EffectBasic.tsx
@@ -1,17 +1,47 @@
-import styles from './EffectBasic.module.css'
+import { useState, useEffect } from "react";
+import styles from "./EffectBasic.module.css";
+
+async function wait(delay = 1000) {
+  return new Promise((resolve) => setTimeout(resolve, delay));
+}
 
 export default function EffectBasic() {
+  const [count, setCount] = useState(0);
+
+  const handleIncreament = async () => {
+    console.log("[1] 상대 업데이트 요청 (Trigger)");
+    await wait(1500);
+    setCount((c) => c + 1);
+  };
+
+  console.log("[2] 상태를 변경한 후, 컴포넌트 컴포넌트 다시 실행 (Render)");
+
+  useEffect(() => {
+    console.log(
+      "[3] 이전, 현재 가상 DOM의 차이 비교: 변경된 부분 DOM에 반영 (Commit)"
+    );
+
+    console.log(
+      "[4] 브라우저는 커밋 이후, 레이아웃을 재종하고, 리페인트(Paint)"
+    );
+    document.title = `현재 카운터: ${count}`;
+  });
+
   return (
     <section className={styles.container}>
       <h2 className={styles.title}>Effect 기초 학습</h2>
 
-      <button type="button" className={styles.counterButton}>
-        카운트 증가: 0
+      <button
+        type="button"
+        className={styles.counterButton}
+        onClick={handleIncreament}
+      >
+        카운트 증가: {count}
       </button>
 
       <p className={styles.statusText}>
         콘솔 로그를 통해 실행 순서를 확인해보세요.
       </p>
     </section>
-  )
+  );
 }
