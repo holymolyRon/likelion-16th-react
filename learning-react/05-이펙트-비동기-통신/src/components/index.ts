b6855b732a3c15c7fba8d7a7e1f02ac5b5715b53
holymolyRon <wjdfhs111@gmail.com>
2026-03-17 10:05:34 +0900

    add: IgnoreStateUpdate 컴포넌트

--- learning-react/05-이펙트-비동기-통신/src/components/index.ts
+++ learning-react/05-이펙트-비동기-통신/src/components/index.ts
@@ -1,14 +1,16 @@
 // 이펙트 기초 (Side Effect 제어)
-export { default as EffectBasic } from './EffectBasic'
+export { default as EffectBasic } from "./EffectBasic";
 
 // 이펙트 의존성(Dependencies)
-export { default as EffectDependencies } from './EffectDependencies'
+export { default as EffectDependencies } from "./EffectDependencies";
 
 // 이펙트 클린업(Cleanup)
-export { default as EffectCleanup } from './EffectCleanup'
+export { default as EffectCleanup } from "./EffectCleanup";
 
 // 데이터 페칭 (Data Fetching)
-export { default as DataFetching } from './DataFetching'
+export { default as DataFetching } from "./DataFetching";
 
 // 경쟁 상태 (Race Condition)
-export { default as RaceCondition } from './RaceCondition'
+export { default as RaceCondition } from "./RaceCondition";
+// 상태 업데이트 무시 (Ignore State Update)
+export { default as IgnoreStateUpdate } from "./IgnoreStateUpdate/IgnoreStateUpdate";
