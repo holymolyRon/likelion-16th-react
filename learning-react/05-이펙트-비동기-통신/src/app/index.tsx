24bf4430b3ce4d4b4cc742e66eda77789e833dd7
holymolyRon <wjdfhs111@gmail.com>
2026-03-17 11:49:45 +0900

    practice: 사용자 ID로 할 일 검색 part 1 - 상태 선언 및 타입 지정 - 데이터 페칭 (API URL 설정) - 조건부 렌더링 (로딩, 데이터 없음, 리스트 렌더링)

--- learning-react/05-이펙트-비동기-통신/src/app/index.tsx
+++ learning-react/05-이펙트-비동기-통신/src/app/index.tsx
@@ -1,10 +1,10 @@
-import { IgnoreStateUpdate } from "@/components";
 import S from "./style.module.css";
+import { TodoSearch } from "@/practices";
 
 export default function App() {
   return (
     <div className={S.container}>
-      <IgnoreStateUpdate />
+      <TodoSearch />
     </div>
   );
 }
