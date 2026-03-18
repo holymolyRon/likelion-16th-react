588d7625a98855981490e5366f61de83782d2ed9
holymolyRon <wjdfhs111@gmail.com>
2026-03-18 14:31:37 +0900

    learn. 확인용 패스워드 입력 유효성 검사

--- learning-react/06-폼-핸들링-DOM-참조/src/components/SmartForm/parts/PasswordConfirmField.tsx
+++ learning-react/06-폼-핸들링-DOM-참조/src/components/SmartForm/parts/PasswordConfirmField.tsx
@@ -1,11 +1,11 @@
-import { useId } from 'react'
-import S from '../SmartForm.module.css'
-import { PasswordInput } from './PasswordInput'
+import { useId, useState } from "react";
+import { PasswordInput } from "./PasswordInput";
+import S from "../SmartForm.module.css";
 
 interface Props {
-  value: string
-  basePassword: string
-  onChange: (val: string) => void
+  value: string;
+  basePassword: string;
+  onChange: (val: string) => void;
 }
 
 // -------------------------------------------------------------------
@@ -27,7 +27,21 @@ export default function PasswordConfirmField({
   basePassword,
   onChange,
 }: Props) {
-  const fieldId = useId()
+  const fieldId = useId();
+  const messageId = useId();
+
+  const [isTouched, setIsTouched] = useState(false);
+
+  const getErrorMessage = () => {
+    if (!isTouched) return "";
+    if (!value) return "확인용 패스워드를 입력해야 합니다.";
+    return value !== basePassword
+      ? "패스워드와 동일한 값을 입력해야 합니다."
+      : "";
+  };
+
+  const error = getErrorMessage();
+  const showError = error !== "";
 
   return (
     <div className={S.field}>
@@ -37,17 +51,22 @@ export default function PasswordConfirmField({
 
       <PasswordInput
         id={fieldId}
+        describeId={messageId}
         value={value}
         onChange={onChange}
+        onBlur={() => setIsTouched(true)}
+        isError={showError}
       />
 
-      {/* <p role="alert" className={S.errorMessage}>
-            {'에러 메시지'}
-          </p> */}
-
-      {/* <p id={messageId} className={S.infoMessage}>
-            패스워드와 동일한 값 입력
-          </p> */}
+      {showError ? (
+        <p role="alert" className={S.errorMessage}>
+          {error}
+        </p>
+      ) : (
+        <p id={messageId} className={S.infoMessage}>
+          패스워드와 동일한 값 입력
+        </p>
+      )}
     </div>
-  )
+  );
 }
