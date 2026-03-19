47657e63dec15d54753fb6de2e21b51117530062
holymolyRon <wjdfhs111@gmail.com>
2026-03-19 14:32:21 +0900

    learn. 하나의 핸들러로 폼 상태 관리 - typeof : 값으로 타입 별칭 생성 - keyof : 타입의 키 값으로 타입 별칭 생성 - 참고 (React): https://ko.react.dev/learn/updating-objects-in-state#using-a-single-event-handler-for-multiple-fields - 참고 (TypeScript): https://www.typescriptlang.org/ko/docs/handbook/2/keyof-types.html#handbook-content | https://www.typescriptlang.org/ko/docs/handbook/2/typeof-types.html#handbook-content

--- learning-react/06-폼-핸들링-DOM-참조/src/app/index.tsx
+++ learning-react/06-폼-핸들링-DOM-참조/src/app/index.tsx
@@ -1,10 +1,10 @@
-import { SmartForm } from '@/components'
-import S from './style.module.css'
+import MultiInputForm from "@/components/MultiInputForm/MultiInputForm";
+import S from "./style.module.css";
 
 export default function App() {
   return (
     <div className={S.container}>
-      <SmartForm />
+      <MultiInputForm />
     </div>
-  )
+  );
 }
