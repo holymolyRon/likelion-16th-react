72245f875e0b09a1127a59b89ba9069c1db4f392
holymolyRon <wjdfhs111@gmail.com>
2026-03-27 11:24:32 +0900

    learn. Context 분리 후 관리 - fast refresh 오류(경고) - 컴포넌트는 컴포넌트만 내보내기가 필요 (빠른 개발을 위해) - 별도로 컨텍스트 관리를 위한 폴더와 파일 생성

--- learning-react/10-Context-API-전역-상태/src/_/learns/ContextBasic/index.tsx
+++ learning-react/10-Context-API-전역-상태/src/_/learns/ContextBasic/index.tsx
@@ -1,51 +1,32 @@
-import { createContext, useState } from 'react'
+import { useState } from "react";
 
-import GrandFather from './parts/GrandFather'
-import S from './style.module.css'
-
-// 2. 컨텍스트 값 타입 정의하기
-interface FamilyContextValue {
-  name: string
-  email: string
-  checked: boolean
-  setName: React.Dispatch<React.SetStateAction<string>>
-  setEmail: React.Dispatch<React.SetStateAction<string>>
-  setChecked: React.Dispatch<React.SetStateAction<boolean>>
-}
-
-// --------------------------------------------------------------------------
-// 코드 작성 흐름
-// --------------------------------------------------------------------------
-// 1. 컨텍스트 생성하기
-// 2. 컨텍스트 값 타입 정의하기
-// 3. 컨텍스트 프로바이더를 통해 컨텍스트 값 공급하기
-// 4. 컨텍스트를 통해 컨텍스트 값 꺼내쓰기
-// --------------------------------------------------------------------------
-
-
-// 1. 컨텍스트 생성하기
-export const FamilyContext = createContext<FamilyContextValue|null>(null)
-
+import { FamilyContext } from "@/contexts/FamilyContext/context";
+import GrandFather from "./parts/GrandFather";
+import S from "./style.module.css";
 
 // --------------------------------------------------------------------------
 
 export default function ContextBasic() {
-  const [name, setName] = useState('박하루')
-  const [email, setEmail] = useState('haru@child.family')
-  const [checked, setChecked] = useState(false)
-  
+  const [name, setName] = useState("박하루");
+  const [email, setEmail] = useState("haru@child.family");
+  const [checked, setChecked] = useState(false);
+
   // 3. 컨텍스트 프로바이더를 통해 컨텍스트 값 공급하기
-  const familyContextValue: FamilyContextValue = {
-    name, email, checked,
-    setName, setEmail, setChecked
-  }
+  const familyContextValue = {
+    name,
+    email,
+    checked,
+    setName,
+    setEmail,
+    setChecked,
+  };
 
   return (
     <FamilyContext.Provider value={familyContextValue}>
       <section className={`${S.box} ${S.container}`}>
         <h1 className={S.title}>깊숙히 컴포넌트 Props 전달</h1>
-          <GrandFather />
+        <GrandFather />
       </section>
     </FamilyContext.Provider>
-  )
+  );
 }
--- learning-react/10-Context-API-전역-상태/src/_/learns/ContextBasic/parts/GrandFather.tsx
+++ learning-react/10-Context-API-전역-상태/src/_/learns/ContextBasic/parts/GrandFather.tsx
@@ -1,20 +1,19 @@
-import { useContext } from 'react'
+import { useContext } from "react";
 
-import grandFatherIcon from '../icons/grand-father.png'
-import { FamilyContext } from '../index'
-import Father from './Father'
-import S from '../style.module.css'
+import { FamilyContext } from "@/contexts/FamilyContext/context";
+import grandFatherIcon from "../icons/grand-father.png";
+import Father from "./Father";
+import S from "../style.module.css";
 
 export default function GrandFather() {
-
   // 컨텍스트 값 사용하기
-  const contextValue = useContext(FamilyContext)
+  const contextValue = useContext(FamilyContext);
 
   return (
     <article>
       <h2 className={`${S.familyTitle} ${S.grandFather}`}>
-        <img src={grandFatherIcon} alt="" width={28} height={28} />{' '}
-        할아버지 (박덕문)
+        <img src={grandFatherIcon} alt="" width={28} height={28} /> 할아버지
+        (박덕문)
       </h2>
       <p>(난 상태만 필요한데...)</p>
 
@@ -26,11 +25,11 @@ export default function GrandFather() {
           <strong>이메일:</strong> {contextValue?.email}
         </p>
         <p>
-          <strong>항렬자 사용:</strong> {contextValue?.checked ? '✅' : '❎'}
+          <strong>항렬자 사용:</strong> {contextValue?.checked ? "✅" : "❎"}
         </p>
       </div>
 
       <Father />
     </article>
-  )
+  );
 }
